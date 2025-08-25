faa4d2872c035c367026fbc8f756d0538923d854
Mks30d <manish30d@gmail.com>
2025-08-25 18:07:41 +0530

    express setup and conncting to mongodb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World from Node API");
+});
+
+mongoose.connect("mongodb+srv://manish30d1999:mks@backenddb.olpr9oy.mongodb.net/NodeJS_API?retryWrites=true&w=majority&appName=BackendDB")
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+    }).catch((err) => {
+        console.log("Error connecting to MongoDB:", err);
+    });
